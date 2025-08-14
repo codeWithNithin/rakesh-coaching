2e3835eff860f8f1be667480f959819a6b9fcbec
codeWithNithin <nithinkottary25@gmail.com>
2025-08-15 01:20:09 +0530

    add global err handler

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,17 @@
-import express from 'express';
-import healthCheckRouter from './routes/health-check.routes.js';
+import express, { type NextFunction, type Request, type Response } from "express";
+import healthCheckRouter from "./routes/health-check.routes.js";
 
 const app = express();
 
-app.use('/health-check', healthCheckRouter);
+app.use("/health-check", healthCheckRouter);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+
+  res.status((err as any).status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
 
 export default app;
